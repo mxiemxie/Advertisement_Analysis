38bcabdb0b3b8067c86f0ba72b5834893bb7e9e4
mxiemxie <mingchentse@gmail.com>
2017-03-01 02:16:53 -0500

    Automaticly tranfer data from file to database and delete file.

--- readTwitterData.py
+++ readTwitterData.py
@@ -2,6 +2,9 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import psycopg2
+import json
+import os
 
 #Variables that contains the user credentials to access Twitter API 
 access_token = "2777671802-F34NQ9bKSYXSfGM05wXNF4hgvGmbybpunmgIpRO"
@@ -16,15 +19,36 @@ class StdOutListener(StreamListener):
     
     
     def on_data(self, data):
-        with open('TwitterData.json', 'a') as f:
 
+        with open('twitterData.json', 'a') as f:
             f.write(data)
             global twitterCount
             twitterCount += 1
-            print (twitterCount)
+            if twitterCount % 10 == 0:
+                self.insert_data('twitterData.json')
+            print twitterCount
             return True
-        # except BaseExeption as e:
-        #     print("BaseExeption")
+
+    def insert_data(self, fileName):
+        conn_string = "host='localhost' dbname='twitterdata' user='mingchen' password='123456'"
+        print "Connecting to database\n ->%s" % (conn_string)
+        conn = psycopg2.connect(conn_string)
+        cursor = conn.cursor()
+        with open(fileName, 'rb') as f:
+                        oriData = f.readlines()
+                        for i in range(len(oriData)):
+                            try:
+                                rowData = json.loads(oriData[i])
+                                text = rowData["text"]
+                                cursor.execute("INSERT INTO twittercontent VALUES (%s);",(text,))
+                            except KeyError:
+                                print "Key Error"
+                            except ValueError:
+                                print "value Error"
+                        conn.commit()
+        print "database update done!    " + fileName
+        os.remove(fileName)
+
 
     def on_error(self, status):
         print status
@@ -38,5 +62,4 @@ if __name__ == '__main__':
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-
     stream.filter(track=['Nike', 'Adidas', 'AJ', 'UA', 'PUMA'])
